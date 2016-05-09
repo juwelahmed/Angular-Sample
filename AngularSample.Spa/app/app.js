cc4e1fa911c4b821c28e187da76b9b9296627dfa
Juwel Ahmed <Juwel Ahmed>
2016-05-09 21:54:53 +0600

    Initial commit

---
+++ AngularSample.Spa/app/app.js
@@ -0,0 +1,40 @@
+﻿var app = angular.module('app', ['ngMessages', 'ngRoute', 'ngAnimate','toaster', 'mgcrea.ngStrap']);
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/', {
+          templateUrl: 'app/shared/view/home.html',
+          controller: 'HomeCtrl'
+      }).
+        when('/contact', {
+            templateUrl: 'app/shared/view/contact.html',
+            controller: 'CotactCtrl'
+        }).
+      when('/student', {
+          templateUrl: 'app/component/student/view/index.html',
+          controller: 'StudentCtrl'
+      }).
+    when('/student/edit/category/:id', {
+        templateUrl: 'app/component/student/view/edit.html',
+        controller: 'StudentEditCtrl'
+    }).
+       when('/student/create', {
+           templateUrl: 'app/component/student/view/edit.html',
+           controller: 'StudentEditCtrl'
+       }).
+      when('/register', {
+          templateUrl: 'app/component/account/view/registerView.html',
+          controller: 'RegisterCtrl'
+      }).
+      otherwise({
+          redirectTo: '/'
+      });
+    $locationProvider.html5Mode(true);
+}]);
+
+app.config(function ($selectProvider) {
+    angular.extend($selectProvider.defaults, {
+        animation: 'am-flip-x',
+        sort: false
+    });
+})
---
+++ AngularSample.Spa/app/component/student/service/StudentService.js
@@ -0,0 +1,38 @@
+﻿angular.module('app').factory('StudentService', function () {
+    var factory = {}
+    var students = [];
+    
+    factory.getStudents = function () {
+        students = [];
+        var student = new Student(1, 'Juwel Ahmed',new Date())
+        students.push(student);
+        student = new Student(2, 'Asraful Islam', new Date())
+        students.push(student);
+        student = new Student(3, 'Zillur Rahman', new Date())
+        students.push(student);
+        student = new Student(4, 'Azharul Islam', new Date())
+        students.push(student);
+
+        return students;
+    }
+
+    factory.getStudent = function (id){
+        if (students.length > 0) {
+            for (var i = 0; i <= students.length - 1; i++) {
+                var student = students[i];
+                if (id == student.id) {
+                    return student;
+                }
+            }
+            return null;
+        }
+    }
+    return factory;
+
+})
+
+function Student(id, name, enrollDate) {
+    this.id = id;
+    this.name = name;
+    this.enrollDate = enrollDate;
+}
